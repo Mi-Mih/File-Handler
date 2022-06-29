bc448fdef27b375e1e09d6653c6fac3659745f99
Mi-Mih <92208626+Mi-Mih@users.noreply.github.com>
2022-06-29 21:38:03 +0300

    Update csv_handlier.py

--- csv_handlier.py
+++ csv_handlier.py
@@ -1,4 +1,13 @@
+from os import listdir
+import csv
+from pickle import dump
+import pandas as pd
+
+
 class FileHandlier:
+    def __init__(self):
+        self.res = {}
+
     # функция определения типа разделителя в csv файле
     def find_delimiter(self, path_file):
         try:
@@ -14,4 +23,31 @@ class FileHandlier:
         except Exception:
             delimiter = 'None'
         return delimiter
+
+    # функция для csv файлов
+    def csv_handlier(self, in_path):
+        csv_files = [i for i in listdir(in_path) if i[len(i) - 3:] == 'csv']
+        for i in csv_files:
+            self.res[i] = pd.read_csv(in_path+'/'+i, sep=self.find_delimiter(in_path + '/' + i), engine='python')
+
+    # функция для эксель файлов
+    def excel_handlier(self, in_path):
+        excel_files = [i for i in listdir(in_path) if i[len(i) - 4:] == 'xlsx']
+        for i in excel_files:
+            self.res[i] = pd.read_excel(in_path + '/' + i)
+
+    def to_pickle(self):
+        with open('Result.pickle', 'wb') as file:
+            dump(self.res, file)
+
+    def main_handlier(self, in_path):
+        self.excel_handlier(in_path)
+        self.csv_handlier(in_path)
+        self.to_pickle()
+
+
+if __name__ == '__main__':
+    path = 'C:/Users/.../PycharmProjects/.../task_data'
+    handlier = FileHandlier()
+    handlier.main_handlier(path)
 
